179ae35f272fd4fdd28efc12fa7d7ac62cb37949
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-05 01:32:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
- if (string === string.toLowerCase());
+ string === string.toLowerCase();
  return `I can't hear you!`
- else if (string === string.toUpperCase());
+ string === string.toUpperCase();
  return `YES INDEED!`
- else if (string === "I love you, Grandma.")
+ string === "I love you, Grandma."
  return `I love you, too.`
 }
